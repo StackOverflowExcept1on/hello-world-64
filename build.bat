53b2e787f7565b19c38ad8b07988c2b2cb2137fa
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-02-28 02:22:49 +0300

    fixed instructions in build.bat

--- build.bat
+++ build.bat
@@ -5,11 +5,6 @@ nasm main.asm -o main.o
 
 REM locate MSVC build tools
 set vswhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
-for /f "usebackq tokens=*" %%i in (``) do (
-    if exist "%%i" set dir=%%i
-)
-
-set vswhere="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
 set vcvarsLookup=call %vswhere% -latest -property installationPath
 
 for /f "tokens=*" %%i in ('%vcvarsLookup%') do set vcvars="%%i\VC\Auxiliary\Build\vcvars64.bat"
